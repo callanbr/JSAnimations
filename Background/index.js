684a62ccefacfdb6fd806c8e7716252a0b51c704
callanbr <chrisallan1992@icloud.com>
2020-07-21 16:10:47 -0700

    background and mouse added

---
+++ Background/index.js
@@ -0,0 +1,67 @@
+c = b.getContext('2d');
+MAX = 900 * 60;
+onload = function update() {
+   requestAnimationFrame(update);
+
+   //init
+   if (!window.time) {
+      time = 0;
+      frame = 0;
+      timeNextFrame = 0;
+      vines = [{ x: 0, y: 0, a: 0, ai: 0, w: 8, p: [], l: MAX }];
+   }
+
+   currentTime = performance.now() / 1000;
+   while (time < currentTime) {
+      while (time < timeNextFrame) {
+         time += 1 / 16384;
+      }
+      frame++;
+      timeNextFrame += 1 / 60;
+
+      //update vine
+      vines = vines.filter((v) => v.l--);
+      vines.forEach((v) => {
+         dx = (Math.cos(v.a) * v.w) / 2;
+         dy = (Math.sin(v.a) * v.w) / 2;
+         v.x += dx;
+         v.y += dy;
+         v.a += v.ai / v.w / 2;
+         v.p.splice(0, v.p.length - 600);
+         v.p.push({ x: v.x, y: v.y, dx: dx, dy: dy });
+         if (frame % 30 == 0) {
+            v.ai = Math.random() - 0.5;
+         }
+         if (v.w > 1 && Math.random() < v.l / 16384 / 2) {
+            vines.push({
+               x: v.x,
+               y: v.y,
+               a: v.a,
+               ai: v.ai,
+               w: v.w / 2,
+               p: [],
+               l: Math.min(v.l, 0 | (v.w * 32 * (1 + Math.random()))),
+            });
+         }
+      });
+   }
+
+   //visuals
+   H = b.height = 512;
+   W = b.width = 0 | ((H * innerWidth) / innerHeight);
+   c.translate(W / 2, H / 2);
+   //c.shadowBlur = 24;
+   vines.forEach((v) => {
+      c.shadowColor = c.strokeStyle =
+         'hsl(' + ((v.a * 60) | 0) + ',100%,' + (60 + v.w * 5) + '%)';
+      if (v.w == 8) {
+         c.translate(-v.x, -v.y);
+      }
+      c.beginPath();
+      l = v.p.length - 1;
+      for (i = l; (p = v.p[i]); i--) {
+         c.lineTo(p.x, p.y);
+      }
+      c.stroke();
+   });
+};
